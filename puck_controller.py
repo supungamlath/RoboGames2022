367af3d06b82e41d6163f72a4c0a08169a64ffda
Supun Gamlath <supungamlath@outlook.com>
2022-11-17 16:44:57 +0530

    Fix error caused by maze not being initialized

--- puck_controller.py
+++ puck_controller.py
@@ -168,6 +168,16 @@ def isWallInfront():
     ):
         return True
 
+def closestColour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.html4_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
 
 ### Motion Functions ###
 def stopMotors():
