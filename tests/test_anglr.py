d8d81cc067bb2e60d253f6f62f8c3050502eed7b
Supun Gamlath <supungamlath@outlook.com>
2022-12-29 21:21:52 +0530

    Add more tests

--- tests/test_anglr.py
+++ tests/test_anglr.py
@@ -3,6 +3,7 @@ sys.path.append('../dave_controller')
 
 from modules.anglr import Angle
 import math
+from maze import Maze
 
 TWO_PI = Angle(0)
 PI = Angle(math.pi)
@@ -31,58 +32,70 @@ QUARTER_PI = Angle(math.pi / 4)
 
 def test_anglr_within():
     angle = Angle(10, "degrees")
-    assert angle.angle_within(QUARTER_PI, 0) == True
+    assert angle.angleWithin(QUARTER_PI, 0) == True
     angle = Angle(-0.8)
     assert angle == TWO_PI - Angle(0.8)
-    assert angle.angle_within(0, TWO_PI - QUARTER_PI) == True
+    assert angle.angleWithin(0, TWO_PI - QUARTER_PI) == True
 
 def test_anglr_between_clockwise():
     start_angle = Angle(0)
     target_angle = QUARTER_PI
-    assert start_angle.angle_between_cw(target_angle) == QUARTER_PI
+    assert start_angle.angleBetweenCW(target_angle) == QUARTER_PI
 
     start_angle = Angle(360, "degrees")
     target_angle = QUARTER_PI
-    assert start_angle.angle_between_cw(target_angle) == QUARTER_PI
+    assert start_angle.angleBetweenCW(target_angle) == QUARTER_PI
 
     start_angle = Angle(10, "degrees")
     target_angle = HALF_PI
-    assert start_angle.angle_between_cw(target_angle) == Angle(80, "degrees")
+    assert start_angle.angleBetweenCW(target_angle) == Angle(80, "degrees")
 
     start_angle = Angle(10, "degrees")
     target_angle = TWO_PI
-    assert start_angle.angle_between_cw(target_angle) == Angle(350, "degrees")
+    assert start_angle.angleBetweenCW(target_angle) == Angle(350, "degrees")
 
     start_angle = Angle(315, "degrees")
     target_angle = TWO_PI
-    assert start_angle.angle_between_cw(target_angle) == QUARTER_PI
+    assert start_angle.angleBetweenCW(target_angle) == QUARTER_PI
 
     start_angle = Angle(315, "degrees")
     target_angle = QUARTER_PI
-    assert start_angle.angle_between_cw(target_angle) == HALF_PI
+    assert start_angle.angleBetweenCW(target_angle) == HALF_PI
 
 def test_anglr_between_counter_clockwise():
     start_angle = Angle(0)
     target_angle = QUARTER_PI
-    assert start_angle.angle_between_ccw(target_angle) == Angle(360 - 45, "degrees")
+    assert start_angle.angleBetweenCCW(target_angle) == Angle(360 - 45, "degrees")
 
     start_angle = Angle(45, "degrees")
     target_angle = Angle(-45, "degrees")
-    assert start_angle.angle_between_ccw(target_angle) == HALF_PI
+    assert start_angle.angleBetweenCCW(target_angle) == HALF_PI
 
     start_angle = TWO_PI
     target_angle = Angle(10, "degrees")
-    assert start_angle.angle_between_ccw(target_angle) == Angle(350, "degrees")
+    assert start_angle.angleBetweenCCW(target_angle) == Angle(350, "degrees")
 
     start_angle = Angle(180, "degrees")
     target_angle = Angle(135, "degrees")
-    assert start_angle.angle_between_ccw(target_angle) == QUARTER_PI
+    assert start_angle.angleBetweenCCW(target_angle) == QUARTER_PI
 
     start_angle = Angle(270, "degrees")
     target_angle = PI
-    assert start_angle.angle_between_ccw(target_angle) == HALF_PI
+    assert start_angle.angleBetweenCCW(target_angle) == HALF_PI
+
+def test_direction_turns():
+    maze = Maze()
+    direction = "right"
+    start_orientation = "E"
+    if direction == "right":
+        expected_bearing = Angle(maze.directionToAngle[start_orientation]) + PI / 2
+
+    elif direction == "left":
+        expected_bearing = Angle(maze.directionToAngle[start_orientation]) - PI / 2
+    assert expected_bearing == Angle(180, "degrees")
 
 if __name__ == '__main__':
     test_anglr_within()
     test_anglr_between_clockwise()
     test_anglr_between_counter_clockwise()
+    test_direction_turns()
--- tests/test_maze.py
+++ tests/test_maze.py
@@ -2,15 +2,13 @@ import sys
 sys.path.append('../dave_controller')
 from maze import Maze
 
-GRID_SIZE = 222
-
 def test_show_maze():
-    m = Maze(GRID_SIZE, GRID_SIZE)
-    # m.loadMaze()
-    # m.showMaze(None, None, refresh_rate = 0)
+    m = Maze()
+    m.loadMaze()
+    m.showMaze(None, None, refresh_rate = 0)
 
 def test_get_cell_in_offset_direction():
-    m = Maze(GRID_SIZE, GRID_SIZE)
+    m = Maze()
     cell = (1, 1)
     offset_cell = m.getCellInOffsetDirection(cell, "N", {"parallel-axis":0, "cross-axis":0})
     assert offset_cell == (1, 1)
@@ -37,4 +35,4 @@ def test_get_cell_in_offset_direction():
 
 if __name__ == "__main__":
     test_show_maze()
-    test_get_cell_in_offset_direction()
+    # test_get_cell_in_offset_direction()
