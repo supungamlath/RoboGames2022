a95708782d09dfc1a43d6f256a92d5f95342b6cc
Supun Gamlath <supungamlath@outlook.com>
2022-12-31 19:37:34 +0530

    New method to analyse training dataset

--- model.py
+++ model.py
@@ -17,6 +17,7 @@ class Model:
         self.y_test = None
         self.features = ["front-right","right-corner","right","left","left-corner","front-left"]
         self.labels=[(2, 2), (2, -2), (0, 3), (0, -3), (1, 3), (1, -3), (2, 3), (2, -3), (3, 1), (3, -1), (3, 0), (3, 2), (3, -2)]
+        # self.labels=[(2, 2), (0, 3), (1, 3), (2, 3), (3, 1), (3, 0), (3, 2), (2, -2), (0, -3), (1, -3), (2, -3), (3, -1), (3, -2)]
         self.classifier = ClassifierChain(
             classifier = RandomForestClassifier(),
             require_dense = [False, True],
@@ -26,25 +27,26 @@ class Model:
         self.testModel()
 
     def trainModel(self):
+        print("Training Multi-label Classification Model")
         df = pandas.read_csv('training_dataset.csv')
         x = df[self.features]
         y = df[["blocked_cells"]]
         data = []
         for index, row in y.iterrows():
-            b_cells = ast.literal_eval(row['blocked_cells'])
+            b_cells = ast.literal_eval(row["blocked_cells"])
             c_row = []
             for c in self.labels:
                 if c in b_cells:
-                    c_row.append(1)
+                    c_row.append(1.0)
                 else:
-                    c_row.append(0)
+                    c_row.append(0.0)
             data.append(c_row)
         y = np.array(pandas.DataFrame(data, columns=self.labels))
 
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x ,y, random_state=100, test_size=0.2, shuffle=True)
         start=time.time()
         self.classifier.fit(self.x_train, self.y_train)
-        print('Training time taken:', round(time.time()-start,2),'seconds')
+        print("Training time taken:'", round(time.time()-start,2),"seconds")
 
     def checkDataSet(self):
         df = pandas.read_csv('training_dataset.csv')
@@ -55,7 +57,7 @@ class Model:
         for index, row in y.iterrows():
             c_row = []
             for c in self.labels:
-                if c in row['blocked_cells']:
+                if c in row["blocked_cells"]:
                     c_row.append(1)
                 else:
                     c_row.append(0)
@@ -72,9 +74,9 @@ class Model:
             row = {}
             for sensor_name in sensor_names:
                 if sensor_name in high_sensors:
-                    row[sensor_name] = random.uniform(160.0, 310.0)
+                    row[sensor_name] = random.uniform(400.0, 1500.0)
                 else:
-                    row[sensor_name] = random.uniform(60.0, 120.0)
+                    row[sensor_name] = random.uniform(60.0, 150.0)
             pprint(row)
 
             row["blocked_cells"] = blocked_cells
@@ -84,9 +86,14 @@ class Model:
         y_hat = self.classifier.predict(self.x_test)
         # print(self.y_test)
         # print(y_hat.toarray())
+        # diff = self.y_test - y_hat.toarray()
+        # diff = pandas.DataFrame(diff, columns=self.labels)
+        # comp = pandas.concat([self.x_test.reset_index(), diff], axis=1)
+        # comp.to_excel('diffs.xlsx', index=False)
+
         cc_f1=metrics.f1_score(self.y_test, y_hat, average='micro')
         cc_hamm=metrics.hamming_loss(self.y_test,y_hat)
-        print('Classifier Chain F1-score:',round(cc_f1,3))
+        print('Classifier Chain F1 Score:',round(cc_f1,3))
         print('Classifier Chain Hamming Loss:',round(cc_hamm,3))
 
     def predict(self, in_features):
@@ -115,6 +122,8 @@ if __name__ == '__main__':
     # model.generateSampleRecord(["front-left", "front-right", "left-corner", "right-corner", "left"], [(3, 2), (3, 1), (3, -1), (3, 0), (3, -2), (0, 3), (1, 3), (2, 3)])
     # model.generateSampleRecord(["left-corner", "left"], [(2, 3), (1, 3), (0, 3)])
     # model.generateSampleRecord(["right-corner", "right"], [(2, -3), (1, -3), (0, -3)])
-    # model.generateSampleRecord(["left-corner", "right"], [(2, 2), (0,-3)])
-    # model.generateSampleRecord(["right-corner", "left"], [(2, -2), (0, 3)])
+    # model.generateSampleRecord(["front-left"], [(3, 1)])
+    # model.generateSampleRecord(["front-right"], [(3, -1)])
+    # model.generateSampleRecord(["left-corner"], [(2, 2)])
+    # model.generateSampleRecord(["right-corner"], [(2, -2)])
     # model.checkDataSet()
